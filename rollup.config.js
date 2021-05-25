fcdd3e2b93ab0f36e7da68ed52958052368ff112
vasco <vastur@gmail.com>
2021-05-25 03:15:32 +0100

    Upgrade postcss

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonJs from '@rollup/plugin-commonjs';
 import postCss from 'rollup-plugin-postcss';
-import postCssSimpleVars from 'postcss-simple-vars';
-import postCssNested from 'postcss-nested';
 import babel from '@rollup/plugin-babel';
 import { terser } from "rollup-plugin-terser";
 import dts from 'rollup-plugin-dts';
@@ -33,12 +31,7 @@ export default [
       }
     ],
     plugins: [
-      postCss({
-        plugins: [
-          postCssSimpleVars(),
-          postCssNested()
-        ]
-      }),
+      postCss(),
       babel({ exclude: 'node_modules/**' }),
       resolve(),
       commonJs()
@@ -61,12 +54,7 @@ export default [
     ],
     external: [...Object.keys(dependencies || {}), ...Object.keys(peerDependencies || {})],
     plugins: [
-      postCss({
-        plugins: [
-          postCssSimpleVars(),
-          postCssNested()
-        ]
-      }),
+      postCss(),
       babel()
     ]
   },
